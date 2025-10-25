1a4a937a2ed6fba69863eb3f902413e466fddd8e
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2025-10-25 23:09:38 +0200

    add DeckBuilders

---
+++ La bataille/Models/DeckBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace La_bataille.Models
+{
+    internal class DeckBuilder
+    {
+        public static List<Cards> GenerateCards()
+        {
+            List<Cards> deck = new List<Cards>();
+
+            foreach (Colors color in Enum.GetValues(typeof(Colors)))
+            {
+                foreach (Values value in Enum.GetValues(typeof(Values)))
+                {
+                    deck.Add(new Cards(value, color));
+                }
+            }
+            return deck;
+        }
+    }
+}
--- La bataille/Program.cs
+++ La bataille/Program.cs
@@ -1,11 +1,11 @@
 ﻿using La_bataille.Models;
 
 // Tests pour vérifier mes méthodes
-Cards carte = new Cards(Values.As, Colors.Pique);
+Cards carte = new (Values.As, Colors.Pique);
 Console.WriteLine(carte);
 Console.WriteLine($"Valeur de la carte : {carte.GetCardValue()}");
 
-Cards carte2 = new Cards(Values.Roi, Colors.Coeur);
+Cards carte2 = new (Values.Roi, Colors.Coeur);
 Console.WriteLine(carte2);
 Console.WriteLine($"Valeur de la carte : {carte2.GetCardValue()}");
 
@@ -18,3 +18,10 @@ else
     Console.WriteLine($"{carte2} est plus forte que {carte}");
 }
 
+// Test pour vérifier les cartes
+List<Cards> deck = DeckBuilder.GenerateCards();
+Console.WriteLine($"Le deck contient {deck.Count} cartes.");
+foreach (Cards card in deck)
+{
+    Console.WriteLine(card);
+}
